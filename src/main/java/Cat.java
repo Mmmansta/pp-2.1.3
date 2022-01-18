cc238898e2942ba0544019854e0902a79b700101
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    fix pom

---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+
+
+    public Cat(){
+
+    }
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
